ab4b49ca18c281779e7879241812a66b33499ced
Gunasekaran19 <113411758+Gunasekaran19@users.noreply.github.com>
2023-05-02 17:14:37 +0530

    Update environment.dart

--- lib/environment/environment.dart
+++ lib/environment/environment.dart
@@ -4,6 +4,6 @@ var clientSecret = "MWUyNWY4MDMtOWE1Zi00Y2E2LTlhYjAtYThkYTA3YjNhOWZi";
 
 //dynamic from gamification Website
 
-var userId = "f8f9b039-7bca-4583-a1fc-867c122bf4ec";
-var applicationId = "6450d7846bb30c8b65548ec9";
-var gameActionId = "6450d7846bb30ce507548ed1";
+var userId = "2d3bb793-3980-42ca-bc40-8c6509ef30d9";
+var applicationId = "6450f769e98691bd85ed44e7";
+var gameActionId = "6450f769e986914531ed44ef";
